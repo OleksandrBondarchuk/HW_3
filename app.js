7f45487fcdc2ac61c036b044a79817bfd095a3d1
OleksandrBondarchuk <102091804+OleksandrBondarchuk@users.noreply.github.com>
2022-10-10 18:11:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let numberFirst = prompt('Please add number #1', 1);
+let numberSecond = prompt('Please add number #2', 2);
+let numberThird = prompt('Please add number #3', 3);
+numberFirst = parseInt(numberFirst);
+numberSecond = parseInt(numberSecond);
+numberThird = parseInt(numberThird);
+let average = (numberFirst + numberSecond + numberThird)/3;
+
+
+alert('Your result: ' + '('  + numberFirst + ' + ' + numberSecond + ' + ' + numberThird + ') / 3 = '  + average);
+
+
+
+
+
